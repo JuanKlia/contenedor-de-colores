98858e11b70f3a4d1ea94ef61485ba3f2d1db570
unknown <juankliabaras@gmail.com>
2022-03-05 18:00:55 -0300

    toggle

--- index.js
+++ index.js
@@ -9,25 +9,22 @@ function main() {
     console.log("tocaron al Rojo");
 
     contenedor.classList.toggle("contenedor-rojo");
-    //contenedor.classList.add("contenedor-rojo");
-    //contenedor.classList.remove("contenedor-verde");
-    //contenedor.classList.remove("contenedor-amarillo");
+    contenedor.classList.remove("contenedor-verde");
+    contenedor.classList.remove("contenedor-amarillo");
   });
   botonAmarillo.addEventListener("click", (e) => {
     console.log("tocaron al Amarillo");
 
     contenedor.classList.toggle("contenedor-amarillo");
-    //contenedor.classList.remove("contenedor-rojo");
-    //contenedor.classList.remove("contenedor-verde");
-    //contenedor.classList.add("contenedor-amarillo");
+    contenedor.classList.remove("contenedor-verde");
+    contenedor.classList.remove("contenedor-rojo");
   });
   botonVerde.addEventListener("click", (e) => {
     console.log("tocaron al Verde");
 
+    contenedor.classList.remove("contenedor-rojo");
+    contenedor.classList.remove("contenedor-amarillo");
     contenedor.classList.toggle("contenedor-verde");
-    //contenedor.classList.remove("contenedor-rojo");
-    //contenedor.classList.remove("contenedor-amarillo");
-    //wcontenedor.classList.add("contenedor-verde");
   });
 }
 
